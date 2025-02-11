fed88226189d10dbddf5495315c3ceafa3588ce4
jakeia <azbazb011@gmail.com>
2025-02-11 11:59:09 +0800

    update

--- lua/user/vscode_keymaps.lua
+++ lua/user/vscode_keymaps.lua
@@ -31,21 +31,24 @@ keymap({ "n", "v" }, "<C-l>", "<cmd>lua require('vscode').action('workbench.acti
 keymap({ "n", "v" }, "<C-j>", "<cmd>lua require('vscode').action('workbench.action.navigateDown')<CR>")
 keymap({ "n", "v" }, "<C-k>", "<cmd>lua require('vscode').action('workbench.action.navigateUp')<CR>")
 -- general keymaps
-keymap({ "n", "v" }, "<leader>ft", "<cmd>lua require('vscode').action('workbench.action.terminal.toggleTerminal')<CR>")
-keymap({ "n", "v" }, "<leader>xx", "<cmd>lua require('vscode').action('workbench.actions.view.problems')<CR>")
+keymap({ "n", "v" }, "<leader>e", "<cmd>lua require('vscode').action('workbench.files.action.focusFilesExplorer')<CR>")
+keymap({ "n", "v" }, "<leader>w", "<cmd>lua require('vscode').action('workbench.action.files.save')<CR>")
+keymap({ "n", "v" }, "]d", "<cmd>lua require('vscode').action('editor.action.marker.nextInFiles')<CR>")
+keymap({ "n", "v" }, "[d", "<cmd>lua require('vscode').action('editor.action.marker.prevInFiles')<CR>")
 keymap({ "n", "v" }, "<leader>db", "<cmd>lua require('vscode').action('editor.debug.action.toggleBreakpoint')<CR>")
-keymap({ "n", "v" }, "<leader>sp", "<cmd>lua require('vscode').action('workbench.actions.view.problems')<CR>")
 keymap({ "n", "v" }, "<leader>cn", "<cmd>lua require('vscode').action('notifications.clearAll')<CR>")
+-- find
 keymap({ "n", "v" }, "<leader>ff", "<cmd>lua require('vscode').action('workbench.action.quickOpen')<CR>")
 keymap({ "n", "v" }, "<leader>fw", "<cmd>lua require('vscode').action('workbench.action.quickTextSearch')<CR>")
 keymap({ "n", "v" }, "<leader>fp", "<cmd>lua require('vscode').action('workbench.action.showCommands')<CR>")
-keymap({ "n", "v" }, "<leader>cr", "<cmd>lua require('vscode').action('code-runner.run')<CR>")
-keymap({ "n", "v" }, "<leader>e", "<cmd>lua require('vscode').action('workbench.files.action.focusFilesExplorer')<CR>")
-keymap({ "n", "v" }, "]d", "<cmd>lua require('vscode').action('editor.action.marker.nextInFiles')<CR>")
-keymap({ "n", "v" }, "[d", "<cmd>lua require('vscode').action('editor.action.marker.prevInFiles')<CR>")
+keymap({ "n", "v" }, "<leader>ft", "<cmd>lua require('vscode').action('workbench.action.terminal.toggleTerminal')<CR>")
+keymap("n", "<leader>fc", [[<cmd>lua require('vscode').action('workbench.action.findInFiles', { args = { query = vim.fn.expand("<cword>") } })<CR>]])
+-- problem
+keymap({ "n", "v" }, "<leader>xx", "<cmd>lua require('vscode').action('workbench.actions.view.problems')<CR>")
+-- tab
 keymap({ "n", "v" }, "<leader>cc", "<cmd>Tabclose<CR>")
 keymap({ "n", "v"}, "<Tab>", "<cmd>Tabnext<CR>")
-keymap({ "n", "v" }, "<leader>w", "<cmd>lua require('vscode').action('workbench.action.files.save')<CR>")
+-- expand keymaps
 keymap({ "n", "v" }, "<cr>", "<cmd>lua require('vscode').action('expand_region')<CR>")
 -- project manager keymaps
 keymap({ "n", "v" }, "<leader>pa", "<cmd>lua require('vscode').action('projectManager.saveProject')<CR>")
@@ -53,6 +56,8 @@ keymap({ "n", "v" }, "<leader>po", "<cmd>lua require('vscode').action('projectMa
 keymap({ "n", "v" }, "<leader>pe", "<cmd>lua require('vscode').action('projectManager.editProjects')<CR>")
 -- dynoFileUtils keymaps
 keymap({ "n", "v" }, "<leader>na", "<cmd>lua require('vscode').action('dynoFileUtils.newItems')<CR>")
+-- code runner keymaps
+keymap({ "n", "v" }, "<leader>cr", "<cmd>lua require('vscode').action('code-runner.run')<CR>")
 -- lsp keymaps
 keymap("n", "K", "<cmd>lua require('vscode-neovim').action('editor.action.showHover')<CR>")
 keymap("n", "gI", "<cmd>lua require('vscode-neovim').action('editor.action.goToImplementation')<CR>")
